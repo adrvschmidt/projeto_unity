4e31838f4949d591dfe4ef4a7e55acc2ff9f4955
Lucas Kaniak <lucasjdk@gmail.com>
2018-11-30 01:06:44 -0200

    unused assets removal, GameKit broken asset bugfix
    Signed-off-by: Lucas Kaniak <lucasjdk@gmail.com>

---
+++ project/game/Assets/Scripts/transitionToGameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Gamekit2D {
+	public class transitionToGameOver : MonoBehaviour {
+
+		void onCollisionEnter(Collider collider) {
+			StartCoroutine(GameOverCoroutine());
+		}
+
+		IEnumerator GameOverCoroutine()
+		{
+			yield return new WaitForSeconds(1.0f); //wait one second before respawing
+			yield return StartCoroutine(ScreenFader.FadeSceneOut(ScreenFader.FadeType.GameOver));
+		}
+	}
+}
+
